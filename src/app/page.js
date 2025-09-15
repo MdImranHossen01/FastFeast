ce73f784ec8b120040628b28d73e9ed96e7ed34f
mustakim67 <mahfujapple95@gmail.com>
2025-09-15 19:01:18 +0600

    CREATED: Created new branch named Mustakim and update inner heading text for first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Welcome to Next.js!</h1>;
+  return <h1>Welcome to Next.js extend efficiency in next level!</h1>;
 }
