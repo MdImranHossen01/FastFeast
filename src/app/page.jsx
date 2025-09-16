6f46de64a755da47bfe9aa6b2e1de570dda6aee7
zingrin <jerinloncheu@gmail.com>
2025-09-16 14:21:23 +0600

    add responsive FAQ section for Fast Feast with toggle functionality and custom colors

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Banner from "./components/Banner";
+import FAQSection from "./components/FaqSection";
 
 export default function Home() {
   return (
     <>
     <Banner/>
-    
+    <FAQSection/>
     </>
   );
 }
