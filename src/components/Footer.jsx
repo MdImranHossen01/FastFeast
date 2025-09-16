bdd08c15c9d0325de4c7396aef413ffdb3f57454
zingrin <jerinloncheu@gmail.com>
2025-09-16 17:58:58 +0600

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>this is footer</h1>
+            <h1 className='text-3xl text-amber-200'>this is footer</h1>
         </div>
     );
 };
