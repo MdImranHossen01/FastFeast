bdf33c6b6e15a8eecb8b5f09ef3ddc86c78dcab5
Captain-Kanak <kanakroy835@gmail.com>
2025-09-18 20:23:02 +0600

    added: dashboard

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,10 +1,30 @@
+import Logo from "@/components/logo";
+import Link from "next/link";
 import React from "react";
+import DashboardLinks from "./components/DashboardLinks";
 
 export default function DashboardLayout({ children }) {
   return (
-    <div>
-      <div>DashboardLayout</div>
-      <div>{children}</div>
+    <div className="container mx-auto">
+      <div className="grid items-center grid-cols-12 border-b border-orange-500">
+        <div className="col-span-3 flex justify-center">
+          <Link href={"/"}>
+            <Logo />
+          </Link>
+        </div>
+
+        <div className="col-span-9 bg-gradient-to-r from-orange-500 to-orange-300 py-5 px-4">
+          <h1 className="text-2xl font-bold text-white">Dashboard</h1>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-12">
+        <div className="col-span-3">
+          <DashboardLinks />
+        </div>
+
+        <div className="col-span-9">{children}</div>
+      </div>
     </div>
   );
 }
