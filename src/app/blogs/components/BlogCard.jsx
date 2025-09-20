67e5fbdac4d0970979fb8f9a64649215d84a0240
Afroza-Nipa13 <afrozaakter228@gmail.com>
2025-09-20 20:37:02 +0600

    added more data & modify blog cards

--- src/app/blogs/components/BlogCard.jsx
+++ src/app/blogs/components/BlogCard.jsx
@@ -7,7 +7,7 @@ import { FaBookOpen } from "react-icons/fa";
 export default function BlogCard({ post }) {
   return (
     <article className="card bg-base-100 shadow-md border border-slate-100 p-2">
-      <figure className="h-40 overflow-hidden">
+      <figure className="h-60 overflow-hidden">
         <img
           src={post.coverImage}
           alt={post.title}
