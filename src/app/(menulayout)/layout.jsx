9080762f06241edc3d969aea3d7055437001c597
MdImranHossen01 <imranshuvo101@gmail.com>
2025-09-25 09:44:37 +0600

    added search bar

---
+++ src/app/(menulayout)/layout.jsx
@@ -0,0 +1,18 @@
+import Banner from "./components/banner";
+import SidebarComponent from "./components/sidebar";
+
+const MenuLayout = ({ children }) => {
+  return (
+    <div>
+      <Banner />
+      <div className="flex flex-12 gap-2">
+        <div className="col-span-4">
+          <SidebarComponent />
+        </div>
+        <div className="col-span-8">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuLayout;
