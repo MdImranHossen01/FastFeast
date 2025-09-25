554285f9c6d1e288200b2ddb81356cb0ceeb7fb0
Captain-Kanak <kanakroy835@gmail.com>
2025-09-25 10:12:55 +0600

    modified: blogs page

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import getBlogs from "../actions/blogs/getBlogs";
 import BlogContent from "./components/BlogContent";
+import { FaHome } from "react-icons/fa";
 
 export const metadata = {
   title: "FastFeast Food Journal – Tradition Meets Delivery",
@@ -10,5 +12,28 @@ export const metadata = {
 export default async function BlogsPage() {
   const posts = await getBlogs();
 
-  return <BlogContent posts={posts} />;
+  return (
+    <div className="max-w-[1500px] mx-auto pt-18">
+      {/* Header  */}
+      <header className="mb-6 flex justify-between items-center space-y-6">
+        <div>
+          <h1 className="text-4xl font-extrabold py-5 bg-gradient-to-r from-red-700 via-orange-400 to-yellow-400 bg-clip-text text-transparent">
+            FastFeast Food Journal – Tradition Meets Delivery
+          </h1>
+          <p className="text-gray-600">
+            Read about Bangladeshi cultural foods, modern delivery trends, and
+            tasty tips for foodies across the nation. Latest tips, rider guides
+            and food ordering news.
+          </p>
+        </div>
+        <Link href={"/"}>
+          <button className="btn btn-outline text-orange-500 rounded-sm flex justify-center hover:bg-orange-600 hover:text-white">
+            <FaHome style={{ marginRight: "8px" }} /> Back to Home
+          </button>
+        </Link>
+      </header>
+
+      <BlogContent posts={posts} />
+    </div>
+  );
 }
