6df319aad7e3f556fa78200a0d06b11e563b777a
MdImranHossen01 <imranshuvo101@gmail.com>
2025-09-26 09:05:44 +0600

    updated

--- src/app/components/SpecialOffers.jsx
+++ src/app/components/SpecialOffers.jsx
@@ -3,13 +3,13 @@ import SpecialOffersSlider from "./SpecialOffersSlider";
 
 export default async function SpecialOffers() {
   const offers = await getOffers();
-
+ 
   return (
-    <section className="py-14 container mx-auto px-4 bg-slate-50">
+    <section className="py-14 container mx-auto px-4 bg-orange-50">
       <div className="text-center mb-12">
-        <h2 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-3">
-          Special Offers
-        </h2>
+        <h2 className="mb-4 text-4xl font-bold text-gray-800">
+          Special <span className="text-orange-500">Offers</span>
+       </h2>
         <p className="text-gray-600 mt-2">
           Delicious deals you don’t want to miss. Limited time only!
         </p>
