7fe1973ed99d927a716982d25d5b5dacc1450d7d
MdImranHossen01 <imranshuvo101@gmail.com>
2025-09-26 09:28:48 +0600

    special section polished

--- src/app/components/SpecialOffers.jsx
+++ src/app/components/SpecialOffers.jsx
@@ -3,20 +3,22 @@ import SpecialOffersSlider from "./SpecialOffersSlider";
 
 export default async function SpecialOffers() {
   const offers = await getOffers();
- 
-  return (
-    <section className="py-14 container mx-auto px-4 bg-orange-50">
-      <div className="text-center mb-12">
-        <h2 className="mb-4 text-4xl font-bold text-gray-800">
-          Special <span className="text-orange-500">Offers</span>
-       </h2>
-        <p className="text-gray-600 mt-2">
-          Delicious deals you don’t want to miss. Limited time only!
-        </p>
-      </div>
 
-      {/* Client slider for interactivity */}
-      <SpecialOffersSlider offers={offers} />
+  return (
+    <section className="py-14 bg-orange-50">
+      <div className="container mx-auto px-4">
+        <div className="text-center mb-12">
+          <h2 className="mb-4 text-4xl font-bold text-gray-800">
+            Special <span className="text-orange-500">Offers</span>
+          </h2>
+          <p className="text-gray-600 mt-2">
+            Delicious deals you don’t want to miss. Limited time only!
+          </p>
+        </div>
+
+        {/* Client slider for interactivity */}
+        <SpecialOffersSlider offers={offers} />
+      </div>
     </section>
   );
 }
