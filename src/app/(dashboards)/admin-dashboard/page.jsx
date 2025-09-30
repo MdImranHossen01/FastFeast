d8263ca7ec4a81ceeea56c6f941697b8877e626c
Captain-Kanak <kanakroy835@gmail.com>
2025-09-30 11:53:50 +0600

    modified: admin dashboard

--- src/app/(dashboards)/admin-dashboard/page.jsx
+++ src/app/(dashboards)/admin-dashboard/page.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import StatsCards from "./components/StatsCards";
 import RecentActivities from "./components/RecentActivities";
@@ -11,8 +10,14 @@ import DeliverySummary from "./components/DeliverySummary";
 import DeliveryStatusSummary from "./components/DeliveryStatusSummary";
 import PopularDishes from "./components/PopularDishes";
 import CustomerFeedback from "./components/CustomerFeedback";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default function AdminHomePage() {
+export default async function AdminHomePage() {
+  const session = await getServerSession(authOptions);
+
+  // console.log(session);
+
   return (
     <div className="p-6 space-y-8">
       <h1 className="text-2xl md:text-3xl font-bold text-gray-800 dark:text-white">
@@ -22,16 +27,16 @@ export default function AdminHomePage() {
       <StatsCards />
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <DeliverySummary/>
-        <DeliveryStatusSummary/>
-        <CustomerFeedback/>
+        <DeliverySummary />
+        <DeliveryStatusSummary />
+        <CustomerFeedback />
         <LiveOrders />
-        <RecentOrders/>
+        <RecentOrders />
         <RecentActivities />
         <TopRestaurants />
         <TopRiders />
         <TrendingFoods />
-        <PopularDishes/>
+        <PopularDishes />
       </div>
     </div>
   );
