8e013dbf630f017a5b3361af15bbcc37fe9962de
julkarzunayed <zunayedjulkar@gmail.com>
2025-10-03 17:46:47 +0600

    Modified manageReview page of admin dashboard

--- src/app/(dashboards)/admin-dashboard/manage-reviews/components/ReviewCards.jsx
+++ src/app/(dashboards)/admin-dashboard/manage-reviews/components/ReviewCards.jsx
@@ -3,7 +3,7 @@ import ReviewCard from "./ReviewCard";
 
 export default function ReviewCards({ reviews }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
       {reviews?.map((review) => (
         <ReviewCard key={review?._id} review={review} />
       ))}
