214d6206e61fc912a7dc183a91cc0e6b3ef21d08
MdImranHossen01 <imranshuvo101@gmail.com>
2025-10-11 23:17:24 +0600

    sadfadf

---
+++ src/app/signup/rider/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RiderSignupPage = () => {
+    return (
+        <div>
+            <h1>this is RiderSignup page</h1>
+        </div>
+    );
+};
+
+export default RiderSignupPage;
