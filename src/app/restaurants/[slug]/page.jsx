87978ada1ca83bdefb166204012afe6ad108c45c
nipaayasha05 <nipaayasha05@gmail.com>
2025-10-11 12:35:18 +0600

    [ADDED]:add restaurant's wise menu in restaurants details page

--- src/app/restaurants/[slug]/page.jsx
+++ src/app/restaurants/[slug]/page.jsx
@@ -1,18 +1,44 @@
-import getMenu from "@/app/actions/restaurant/getRestaurant";
 import React from "react";
 import { generateSlug } from "../components/generateSlug";
 
 export default async function RestaurantDetails({ params }) {
   const { slug } = await params;
-  const restaurants = await getMenu();
-  console.log(restaurants);
 
-  //   find individual restaurant
+  // fetch all approved restaurants
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/api/restaurant?status=approved`
+  );
+  const restaurants = await res.json();
+
+  //   find restaurant by slug
   const restaurant = restaurants.find(
     (restaurant) =>
       generateSlug(restaurant.name, restaurant.location?.area) === slug
   );
+
+  if (!restaurant) {
+    return <div>Restaurant not found</div>;
+  }
+
+  // fetch restaurant-wise menu from foods collection
+  const menuRes = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/api/restaurant/${restaurant._id}`
+  );
+
+  const { menus } = await menuRes.json();
+
+  console.log("Menus:", menus);
+
   console.log(restaurant);
 
-  return <div>RestaurantDetails</div>;
+  return (
+    <div>
+      {menus.map((menu) => (
+        <div key={menu._id}>
+          {menu.title}{" "}
+          <img className="w-[300px]" src={menu.imageUrl} alt="menu" />
+        </div>
+      ))}
+    </div>
+  );
 }
