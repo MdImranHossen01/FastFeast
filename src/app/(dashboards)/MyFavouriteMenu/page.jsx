c4f10cebe610549e188d1726ffd880d77545f81d
MdImranHossen01 <imranshuvo101@gmail.com>
2025-10-12 09:54:45 +0600

    fixing error

---
+++ src/app/(dashboards)/MyFavouriteMenu/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyFavouriteMenuPage = () => {
+  return (
+    <div>
+      <h1>This is my Favourite Menu page</h1>
+    </div>
+  );
+};
+
+export default MyFavouriteMenuPage;
