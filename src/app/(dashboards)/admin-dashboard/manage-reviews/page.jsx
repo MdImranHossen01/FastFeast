89f43ad72936fe7fb6604568898b83ac61491c32
julkarzunayed <zunayedjulkar@gmail.com>
2025-10-14 19:19:04 +0600

    updated API and added data get methode

--- src/app/(dashboards)/admin-dashboard/manage-reviews/page.jsx
+++ src/app/(dashboards)/admin-dashboard/manage-reviews/page.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { reviews } from "./reviews";
 import ManageReviewHeader from "./components/ManageReviewHeader";
 import ReviewCards from "./components/ReviewCardContainer";
 import ReviewCardContainer from "./components/ReviewCardContainer";
 import ReviewTable from "./components/ReviewTable";
+import getReviews from "@/app/actions/reviews/getReviews";
 
 export default async function ManageReviews({ searchParams }) {
-  const { search } = await searchParams;
-  const data = reviews.filter((d) =>
-    search === "All-reviews" ? true : d.targetType === search
-  );
+  const { search, ratings } = await searchParams;
+
+
+  const data = await getReviews(search, ratings);
+  // console.log(data)
 
   return (
     <div className="px-6">
       <ManageReviewHeader />
-      <ReviewTable reviews={data}/>
+      <ReviewTable reviews={data} />
       {/* <ReviewCardContainer reviews={data} /> */}
     </div>
   );
--- src/app/api/reviews/route.js
+++ src/app/api/reviews/route.js
@@ -1,10 +1,32 @@
 import { collectionsName, dbConnect } from "@/lib/dbConnect";
 
-export async function GET() {
+export async function GET(req) {
     try {
+        const request = await req
+
+        const { searchParams } = new URL(request?.url);
+        //http://localhost:3000/admin-dashboard/manage-reviews?search=All-reviews&ratings=All-ratings
+        const search = searchParams.get("search");
+        const rating = searchParams.get("ratings");
+
+        const filter = {}
+
+
+        const ratingsValue = parseInt(rating);
+        if (!isNaN(ratingsValue) && ratingsValue >= 1 && ratingsValue <= 5) {
+            filter.rating = ratingsValue;
+        }
+
+        const searchStrings = ["All-reviews", "Rider", "Restaurant", "Food"];
+        if (searchStrings.includes(search)) {
+            filter.targetType = search;
+        }
+        // console.log({ search, rating });
+
         // Connect reviews collection
+
         const reviewsCollection = await dbConnect(collectionsName.reviewsCollection);
-        const reviews = await reviewsCollection.find().toArray();
+        const reviews = await reviewsCollection.find(filter).toArray();
 
         return Response.json(reviews, { status: 200 });
     } catch (error) {
@@ -26,6 +48,24 @@ export async function POST(req) {
             { ...result },
             { status: 201 }
         )
+    } catch (error) {
+        return Response.json(
+            { success: false, message: error.message },
+            { status: 500 }
+        )
+    }
+}
+export async function DELETE(req) {
+    try {
+        // const review = await req.json();
+
+        // const reviewsCollection = await dbConnect(collectionsName.reviewsCollection);
+        // const result = await reviewsCollection.deleteMany({});
+
+        // return Response.json(
+        //     { ...result },
+        //     { status: 201 }
+        // )
     } catch (error) {
         return Response.json(
             { success: false, message: error.message },
