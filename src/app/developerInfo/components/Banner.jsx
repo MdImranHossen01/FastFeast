4f64c5d124ae19645fc2b0970abca3890b5fca01
julkarzunayed <zunayedjulkar@gmail.com>
2025-10-28 09:55:49 +0600

    modified peveloper page banner

--- src/app/developerInfo/components/Banner.jsx
+++ src/app/developerInfo/components/Banner.jsx
@@ -15,27 +15,34 @@ export default function Banner() {
     return (
         <section
             style={{
-                // backgroundImage: `url(/code_png.png)`
+                backgroundImage: `url(/paper-texture.jpg)`,
             }}
-            className='min-h-screen bg-center bg-no-repeat flex justify-center items-center text-9xl  '>
+            className='min-h-screen  bg-fixed bg-cover bg-no-repeat '>
             <div
                 style={{
-                    backgroundImage: `url(/sl_031420_28950_10.jpg)`
+                    backgroundImage: `url(/code-line.png)`,
+                    // backgroundPosition: '-100px'
                 }}
-                className="bg-animation-text bg-no-repeat text-transparent bg-clip-text  bg-cover  border-red-600 p-5">
-                <h1
-                    // style={{
-                    //     color: 'transparent',
-                    //     // textShadow: '2px 2px #ff0000',
-                    //     textShadow: '1px 1px 0 #00FF00, -1px -1px 0 #00FF00, 1px -1px 0 #00FF00, -1px 1px 0 #00FF00, 0 0 10px rgba(0, 255, 0, 0.7)',
-                    // }}
-                    className={`${rakkas.className} text-center text-6xl sm:text-7xl md:text-9xl bg-clip-border  bg-linear-to- from-violet-500 to-cyan-500`}>
-                    <span className="text-4xl sm:text-5xl md:text-6xl">
-                        The
-                    </span>
-                    <br />
-                    Developers
-                </h1>
+                className="min-h-screen bg-fixed bg-size-[auto_350px] bg-bottom-right bg-no-repeat flex justify-center items-center text-9xl ">
+                <div
+                    style={{
+                        backgroundImage: `url(/sl_031420_28950_10.jpg)`
+                    }}
+                    className="bg-animation-text bg-no-repeat text-transparent bg-clip-text  bg-cover  border-red-600 p-5">
+                    <h1
+                        // style={{
+                        //     color: 'transparent',
+                        //     // textShadow: '2px 2px #ff0000',
+                        //     textShadow: '1px 1px 0 #00FF00, -1px -1px 0 #00FF00, 1px -1px 0 #00FF00, -1px 1px 0 #00FF00, 0 0 10px rgba(0, 255, 0, 0.7)',
+                        // }}
+                        className={`${rakkas.className} text-center text-6xl sm:text-7xl md:text-9xl bg-clip-border  bg-linear-to- from-violet-500 to-cyan-500`}>
+                        <span className="text-4xl sm:text-5xl md:text-6xl">
+                            The
+                        </span>
+                        <br />
+                        Developers
+                    </h1>
+                </div>
             </div>
         </section>
     )
