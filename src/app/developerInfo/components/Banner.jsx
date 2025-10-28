225a4f73b4bde56fbada2294c87c722bed4c8b32
julkarzunayed <zunayedjulkar@gmail.com>
2025-10-28 07:05:48 +0600

    added developers info cards

--- src/app/developerInfo/components/Banner.jsx
+++ src/app/developerInfo/components/Banner.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
 import { Rakkas } from "next/font/google";
+import "../style.css"
 
 const rakkas = Rakkas({
     weight: ["400"],
     subsets: ["latin"],
 });
 
+//bg-size-[auto_500%]
+//sl_031420_28950_10.jpg
+///computer-program-coding-screen.jpg
+
 export default function Banner() {
     return (
-        <section className='min-h-screen flex justify-center items-center text-9xl text-orange-600 '>
-            <h1 className={`${rakkas.className} `}>Meet the Developers</h1>
+        <section
+            style={{
+                // backgroundImage: `url(/code_png.png)`
+            }}
+            className='min-h-screen bg-center bg-no-repeat flex justify-center items-center text-9xl  '>
+            <div
+                style={{
+                    backgroundImage: `url(/sl_031420_28950_10.jpg)`
+                }}
+                className="bg-animation-text bg-no-repeat text-transparent bg-clip-text  bg-cover  border-red-600 p-5">
+                <h1
+                    // style={{
+                    //     color: 'transparent',
+                    //     // textShadow: '2px 2px #ff0000',
+                    //     textShadow: '1px 1px 0 #00FF00, -1px -1px 0 #00FF00, 1px -1px 0 #00FF00, -1px 1px 0 #00FF00, 0 0 10px rgba(0, 255, 0, 0.7)',
+                    // }}
+                    className={`${rakkas.className} text-center text-6xl sm:text-7xl md:text-9xl bg-clip-border  bg-linear-to- from-violet-500 to-cyan-500`}>
+                    <span className="text-4xl sm:text-5xl md:text-6xl">
+                        The
+                    </span>
+                    <br />
+                    Developers
+                </h1>
+            </div>
         </section>
     )
 }
