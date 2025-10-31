2cffc35a7f0b2e5ffe4207d5db665bdf292450a8
Afroza-Nipa13 <afrozaakter228@gmail.com>
2025-10-31 07:29:56 +0600

    optimized images from several pages & rearranged file structer of career page

--- src/app/careers/page.jsx
+++ src/app/careers/page.jsx
@@ -1,12 +1,13 @@
 "use client"
 
+import { jobs } from "../api/jobs/jobs";
 import Dropdown from "./components/Dropdown";
 import JobList from "./components/JobList";
 import JobsBanner from "./components/JobsBanner";
 
 
 
-export default function JobsContainer({jobs}) {
+export default function JobsContainer() {
  
 
   
